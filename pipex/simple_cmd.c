78760648a0cd70469d2cd6619c7d81b255b320de
Raflox <rafaeldgamasilva@gmail.com>
2023-09-07 13:11:08 +0100

    first try to implement pipex on main

--- pipex/simple_cmd.c
+++ pipex/simple_cmd.c
@@ -6,7 +6,7 @@
 /*   By: rafilipe <rafilipe@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:55:04 by rafilipe          #+#    #+#             */
-/*   Updated: 2023/09/04 21:16:51 by rafilipe         ###   ########.fr       */
+/*   Updated: 2023/09/07 12:34:33 by rafilipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ typedef struct s_cmd
 	char	*infile;
 	char	*outfile;
 	char	**env;
+	char	*red_type;
+	bool	is_built_in;
 }	t_cmd;
 
 int	file_ctl(char *filename, int mode)
@@ -32,6 +34,8 @@ int	file_ctl(char *filename, int mode)
 		}
 		return (open(filename, O_RDONLY));
 	}
+	if (mode == APPEND)
+		open(filename, O_RDWR | O_CREAT | O_APPEND, 0644);
 	return (open(filename, O_RDWR | O_CREAT | O_TRUNC, 0644));
 }
 
@@ -42,21 +46,28 @@ void	simple_cmd(t_cmd cmd_struct)
 	char	*cmd;
 	int		pid;
 
-/* 	if (ac == 2)
-		execute(av[1], env); */
-	if (*cmd_struct.infile)
+	if (*cmd_struct.infile) //get infile from first element of list
 	{
 		in_fd = file_ctl(cmd_struct.infile, INFILE);
 		dup2(in_fd, STDIN_FILENO);
 	}
-	if (*cmd_struct.outfile)
+	if (*cmd_struct.outfile) //get outfile from last element of list
 	{
+		if (cmd_struct.red_type == "append")
+			out_fd = file_ctl(cmd_struct.outfile, APPEND);
 		out_fd = file_ctl(cmd_struct.outfile, OUTFILE);
 		dup2(out_fd, STDOUT_FILENO);
 	}
-	pid = fork();
-	if (pid == 0)
-		execute(cmd_struct.cmd, cmd_struct.env);
+	if (cmd_struct.is_built_in)
+		printf("AQUI\n"); // run builtin function
+	process_ctl(av[2], env, fd[0]); // Redirect and execute the first command.
+	// FIXME: ADD CONDITION HERE TO VERIFY IF THERE IS MORE THAN ONE COMMAND TO BE EXECUTED
+	while (i < ac - 2) //alterar iteração para listas
+	{
+		fprintf(stderr, "arg:%d\n", i);
+		process_ctl(av[i++], env, 1); // Loop to redirect and execute subsequent commands.
+	}
+	execute(av[i], env); // Execute the final command.
 	close(in_fd);
 	close(out_fd);
 }
@@ -69,6 +80,8 @@ int	main(int ac, char **av, char **env)
 	cmd_struct.infile = "test.txt";
 	cmd_struct.outfile = "outfile";
 	cmd_struct.env = env;
+	cmd_struct.is_built_in = true;
+	cmd_struct.red_type = "append";
 
 	simple_cmd(cmd_struct);
 }

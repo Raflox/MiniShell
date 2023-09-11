e2b22ccec3310c6a17cb38cfe66c5cbb7bea1ba0
Raflox <rafaeldgamasilva@gmail.com>
2023-09-11 16:31:18 +0100

    pipex integration

---
+++ include/pipex.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rafilipe <rafilipe@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 15:24:11 by rafilipe          #+#    #+#             */
+/*   Updated: 2023/09/11 16:19:13 by rafilipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include "minishell.h"
+# include "../lib/libft/include/libft.h"
+
+# define INFILE 0
+# define OUTFILE 1
+# define APPEND 2
+
+typedef struct s_pipex {
+	int		fd[2];
+	pid_t	pid;
+	int		pipe_fd[2];
+}	t_pipex;
+
+/* UTILS */
+void	error(char *loc);
+void	execute(char *cmd, char **env);
+
+/* MAIN */
+void	simple_cmd(t_seg cmd_struct);
+
+#endif
